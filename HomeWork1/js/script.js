544c6151ea5ef7c01e5196648a801ee4d7ea4b4b
Max <max.zaharow02@yandex.ru>
2022-07-31 18:14:04 +0300

    created a movieDB object

--- HomeWork1/js/script.js
+++ HomeWork1/js/script.js
@@ -1,2 +1,12 @@
+"use strict";
+
 const numberOfFilms = prompt("How many films did you watch?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
