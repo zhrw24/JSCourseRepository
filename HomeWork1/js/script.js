118b743d0480e6873f61b8d1cef052a01e0ff60d
Max <max.zaharow02@yandex.ru>
2022-08-04 21:16:12 +0300

    more beauty

--- HomeWork1/js/script.js
+++ HomeWork1/js/script.js
@@ -1,42 +1,5 @@
 "use strict";
 
-
-
-// function start() {
-//     while(numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)){
-//         numberOfFilms = prompt("How many films did you watch?", '');
-//     }
-// }
-
-// function rememberMyFilms() {
-//     for(let i = 0; i < 2; i++){
-//         let nameOfFilm = prompt('Введите название фильма', '');
-//         let filmRating = +prompt('Дайте оценку фильму', '');  
-//         if(nameOfFilm.length > 50 || nameOfFilm == '' || filmRating < 0 || filmRating > 10 || isNaN(filmRating)){
-//             i--;
-//             alert('Error');
-//         }else{
-//             personalMovieDB.movies[nameOfFilm] = filmRating;
-//             alert('done');
-//         }
-//     }
-// }
-
-// function detectPersonalLevel() {
-//     if(personalMovieDB.count < 10){
-//         alert("Вы смотрели довольно мало фильмов");
-//     }else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-//         alert("Вы - среднестатистический зритель");
-//     }else if(personalMovieDB.count >= 30){
-//         alert("Вы - киноман");
-//     }else{
-//         alert('Error');
-//     }
-// }
-
-
-
-
 let numberOfFilms;
 
 const personalMovieDB = {
@@ -93,20 +56,6 @@ const personalMovieDB = {
         }
     }
 };
-
-// function showMyDB() {
-//     if(personalMovieDB.private == false) {
-//         console.log(personalMovieDB);
-//     }else {
-//         console.log('Failed!This DB is private!');
-//     }
-// }
-
-// function writeYourGenres() {
-//     for(let i = 0; i < 3; i++) {
-//         personalMovieDB.genres[i] = prompt(`Your favorite genre #${ i + 1 }`, ``);
-//     }
-// }
 
 personalMovieDB.start();
 
